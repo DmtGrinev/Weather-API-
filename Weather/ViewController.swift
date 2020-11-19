69e31be303d842ed3093d676993aea0a8e0dd7c5
Dmitry grinev <grinev.dmt@gmail.com>
2020-11-19 20:35:38 +0500

    update interface

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -14,6 +14,7 @@ import UIKit
     @IBOutlet var temperatureLabel: UILabel!
     @IBOutlet var feelingTemperature: UILabel!
     @IBOutlet var seachButton: UIButton!
+    @IBOutlet var cityLabel: UILabel!
     
     
     var netWorkManager = NetWorkManager()
@@ -22,44 +23,60 @@ import UIKit
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        temperatureLabel.text = "ºC"
-        feelingTemperature.text = "Feels like  ºC"
+        //        temperatureLabel.text = "ºC"
+        //        feelingTemperature.text = "Feels like  ºC"
         
-        netWorkManager.fetchCurrentWeather(forCity: "London")
+        netWorkManager.onComplition = { [weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterfaceWith(weather: currentWeather)
+        }
+        netWorkManager.fetchCurrentWeather(forCity: "Tyumen")
     }
     
     
     
     
     @IBAction func seachActionButton(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) {
+            [unowned self] city in
             self.netWorkManager.fetchCurrentWeather(forCity: city)
         }
+        
     }
     
     
-        func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style, completionHandler: @escaping (String) -> Void) {
-            let alertVC = UIAlertController(title: title, message: message, preferredStyle: style)
-            alertVC.addTextField { tf in
-                let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
-                tf.placeholder = cities.randomElement()
-            }
-            let search = UIAlertAction(title: "Search", style: .default) { action in
-                let textField = alertVC.textFields?.first
-                guard let cityName = textField?.text else { return }
-                if cityName != "" {
-                    let city = cityName.split(separator: " ").joined(separator: "%20")
-                    completionHandler(city)
-                }
-            }
-            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
-            alertVC.addAction(search)
-            alertVC.addAction(cancel)
-            present(alertVC, animated: true, completion: nil)
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style, completionHandler: @escaping (String) -> Void) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: style)
+        alertVC.addTextField { textField in
+            let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
+            textField.placeholder = cities.randomElement()
         }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = alertVC.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                let city = cityName.split(separator: " ").joined(separator: "%20")
+                completionHandler(city)
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertVC.addAction(search)
+        alertVC.addAction(cancel)
+        present(alertVC, animated: true, completion: nil)
     }
     
+    func updateInterfaceWith (weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = "\(weather.temperatureString) Cº"
+            self.feelingTemperature.text = "Feels like \(weather.feelsLikeTemperatureString) Cº"
+            self.weatherImage.image = UIImage(systemName: weather.systemIconNameString)
+        }
+        
+    }
+}
+
 
 
 
