3f3e9c02a97138f6fe05d4569fd9f96f8c0c59da
Dmitry grinev <grinev.dmt@gmail.com>
2020-11-19 15:11:44 +0500

    parseJSON

---
+++ Weather/NetWorkDataFiles/CurrentWeather.swift
@@ -0,0 +1,32 @@
+//
+//  CurrentWeather.swift
+//  Weather
+//
+//  Created by Dmitry Grinev on 19.11.2020.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    
+    let cityName: String
+    
+    let temperature: Double
+    var temperatureString: String {
+        return "\(temperature.rounded())"
+    }
+    
+    let feelsLikeTemperature: Double
+    var feelsLikeTemperatureString: String {
+        return "\(feelsLikeTemperature)"
+    }
+    
+    let conditionCode: Int
+    
+    init?(currentWeatherData: CurrentWeatherData) {
+        cityName = currentWeatherData.name
+        temperature = currentWeatherData.main.temp
+        feelsLikeTemperature = currentWeatherData.main.feelsLike
+        conditionCode = currentWeatherData.weather.first!.id
+    }
+}
